cffcaef51c908629f62017e0ddd48c35961e5e08
alam1906 <107329748+alam1906@users.noreply.github.com>
2025-01-14 03:58:42 -0800

    notifikasi bookmark

--- lib/controllers/surat_controller.dart
+++ lib/controllers/surat_controller.dart
@@ -2,35 +2,9 @@ import 'dart:convert';
 
 import 'package:al_quran/models/detail_surat_model.dart';
 import 'package:al_quran/models/surat_model.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/services.dart';
 
 class SuratController {
-  Dio dio = Dio(
-    BaseOptions(
-      baseUrl: 'https://equran.id/api/v2',
-    ),
-  );
-  Future<List<SuratModel>> getAllSurat() async {
-    final response = await dio.get('/surat');
-    final data = response.data as Map<String, dynamic>;
-
-    List<SuratModel> allSurat = [];
-    for (var e in data['data']) {
-      allSurat.add(SuratModel.fromJson(e));
-    }
-
-    return allSurat;
-  }
-
-  Future<DetailSuratModel> getDetailSurat(int nomor) async {
-    final response = await dio.get('/surat/$nomor');
-    final data = response.data as Map<String, dynamic>;
-    final result = data['data'] as Map<String, dynamic>;
-    final akhir = DetailSuratModel.fromJson(result);
-    return akhir;
-  }
-
   Future<List<SuratModel>> getAllSuratLokal() async {
     final String response =
         await rootBundle.loadString('assets/json/listsurat.json');
